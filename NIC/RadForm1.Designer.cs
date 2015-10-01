7573f3e18d2d93affc2ffd2b6cccd35fac321de8
Ajith <ajithsri@gmail.com>
2015-10-01 13:26:49 +0530

    Initial commit

---
+++ NIC/RadForm1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace NIC
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadForm1));
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.txtnic = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.txterror = new Telerik.WinControls.UI.RadLabel();
+            this.txtdob = new Telerik.WinControls.UI.RadLabel();
+            this.txtgender = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel6 = new Telerik.WinControls.UI.RadLabel();
+            this.picGender = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txterror)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdob)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtgender)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGender)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radLabel1.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel1.Location = new System.Drawing.Point(21, 35);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(73, 25);
+            this.radLabel1.TabIndex = 1;
+            this.radLabel1.Text = "NIC No";
+            // 
+            // radButton1
+            // 
+            this.radButton1.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radButton1.Location = new System.Drawing.Point(386, 24);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(89, 40);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "Find Info";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // txtnic
+            // 
+            this.txtnic.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnic.Location = new System.Drawing.Point(147, 30);
+            this.txtnic.MaxLength = 9;
+            this.txtnic.Name = "txtnic";
+            this.txtnic.Size = new System.Drawing.Size(184, 27);
+            this.txtnic.TabIndex = 0;
+            this.txtnic.TextChanged += new System.EventHandler(this.txtnic_TextChanged);
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.Location = new System.Drawing.Point(21, 97);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(114, 25);
+            this.radLabel2.TabIndex = 1;
+            this.radLabel2.Text = "Date of Birth";
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel3.Location = new System.Drawing.Point(21, 158);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(69, 25);
+            this.radLabel3.TabIndex = 2;
+            this.radLabel3.Text = "Gender";
+            // 
+            // txterror
+            // 
+            this.txterror.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txterror.ForeColor = System.Drawing.Color.DarkRed;
+            this.txterror.Location = new System.Drawing.Point(146, 64);
+            this.txterror.Name = "txterror";
+            this.txterror.Size = new System.Drawing.Size(2, 2);
+            this.txterror.TabIndex = 1;
+            // 
+            // txtdob
+            // 
+            this.txtdob.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdob.Location = new System.Drawing.Point(147, 123);
+            this.txtdob.Name = "txtdob";
+            this.txtdob.Size = new System.Drawing.Size(2, 2);
+            this.txtdob.TabIndex = 3;
+            // 
+            // txtgender
+            // 
+            this.txtgender.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtgender.Location = new System.Drawing.Point(147, 184);
+            this.txtgender.Name = "txtgender";
+            this.txtgender.Size = new System.Drawing.Size(2, 2);
+            this.txtgender.TabIndex = 4;
+            // 
+            // radLabel6
+            // 
+            this.radLabel6.Font = new System.Drawing.Font("Bell MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel6.Location = new System.Drawing.Point(337, 31);
+            this.radLabel6.Name = "radLabel6";
+            this.radLabel6.Size = new System.Drawing.Size(22, 25);
+            this.radLabel6.TabIndex = 5;
+            this.radLabel6.Text = "V";
+            // 
+            // picGender
+            // 
+            this.picGender.Location = new System.Drawing.Point(115, 238);
+            this.picGender.Name = "picGender";
+            this.picGender.Size = new System.Drawing.Size(266, 212);
+            this.picGender.TabIndex = 0;
+            this.picGender.TabStop = false;
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 467);
+            this.Controls.Add(this.picGender);
+            this.Controls.Add(this.radLabel6);
+            this.Controls.Add(this.txtgender);
+            this.Controls.Add(this.txtdob);
+            this.Controls.Add(this.txterror);
+            this.Controls.Add(this.radLabel3);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.txtnic);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.radLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "NIC Information";
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtnic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txterror)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtdob)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtgender)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGender)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadTextBox txtnic;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadLabel txterror;
+        private Telerik.WinControls.UI.RadLabel txtdob;
+        private Telerik.WinControls.UI.RadLabel txtgender;
+        private Telerik.WinControls.UI.RadLabel radLabel6;
+        private System.Windows.Forms.PictureBox picGender;
+    }
+}
---
+++ NIC/RadForm1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace NIC
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            Calcuulator();
+        }
+
+        private void txtnic_TextChanged(object sender, EventArgs e)
+        {
+            if(txtnic.TextLength==9)
+            {
+                Calcuulator();
+            }
+            else
+            {
+                txtdob.Text = "";
+                txtgender.Text = "";
+                txterror.Text = "";
+                picGender.ImageLocation = "";
+            }
+        }
+
+        private void Calcuulator()
+        {
+            String sNICNo = txtnic.Text;//Getting The Entered NIC Number 
+            String sYear = "";
+            String sMonth = "";
+            int iDay = 0;
+            string gender = "";
+
+            if (sNICNo.Length == 9)//Validate 1::Checking The Length Of The NIC Entered
+            
+            {
+                sNICNo = sNICNo.Substring(0, 9);
+                bool bchk9 = Int32.TryParse(sNICNo, out iDay);
+                if (bchk9)//Validate 2::Checking The First Nine Digit Of The NIC Entered Are Integers
+                {
+                    sYear = "19"+sNICNo.Substring(0, 2);//Saving The Year
+                    sNICNo = sNICNo.Substring(2, 3);
+                    iDay = int.Parse(sNICNo);
+                    if (iDay > 500)
+                    {
+                        iDay = iDay - 500;
+                        gender = "Female";
+                    }
+                    else
+                    {
+                        gender = "Male";
+                    }
+                    if (iDay > 366 || iDay == 0)//Validate 3::Validating The Days In The Entered NIC
+                    {
+                        txterror.Text = "Please Enter A Valid NIC No";
+                    }
+                    else
+                    {
+                        if (iDay > 335)
+                        {
+                            iDay = iDay - 335;
+                            sMonth = "December";
+                        }
+                        else if (iDay > 305)
+                        {
+                            iDay = iDay - 305;
+                            sMonth = "November";
+                        }
+                        else if (iDay > 274)
+                        {
+                            iDay = iDay - 274;
+                            sMonth = "October";
+                        }
+                        else if (iDay > 244)
+                        {
+                            iDay = iDay - 244;
+                            sMonth = "September";
+                        }
+                        else if (iDay > 213)
+                        {
+                            iDay = iDay - 213;
+                            sMonth = "Auguest";
+                        }
+                        else if (iDay > 182)
+                        {
+                            iDay = iDay - 182;
+                            sMonth = "July";
+                        }
+                        else if (iDay > 152)
+                        {
+                            iDay = iDay - 152;
+                            sMonth = "June";
+                        }
+                        else if (iDay > 121)
+                        {
+                            iDay = iDay - 121;
+                            sMonth = "May";
+                        }
+                        else if (iDay > 91)
+                        {
+                            iDay = iDay - 91;
+                            sMonth = "April";
+                        }
+                        else if (iDay > 60)
+                        {
+                            iDay = iDay - 60;
+                            sMonth = "March";
+                        }
+                        else if (iDay < 32)
+                        {
+                            sMonth = "January";
+                        }
+                        else if (iDay > 31)
+                        {
+                            iDay = iDay - 31;
+                            sMonth = "Febuary";
+                        }
+
+                        txtdob.Text = iDay + " - " + sMonth + " - " + sYear;
+                        txtgender.Text = gender;
+                        picGender.ImageLocation = gender == "Male" ? "male.png" : "female.png"; 
+                    }                    
+                }
+                else
+                {
+                    txterror.Text = "Please Enter A Valid NIC No";
+                }
+            }
+            else
+            {
+                txterror.Text = "Please Enter A Valid NIC No";
+            }
+        }
+    }
+}
